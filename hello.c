6f7af8e3b0bb219360033bdce1fe5060d93bc2b9
Isaac Trausch <isaactrausch2@huskers.unl.edu>
2026-08-27 10:46:44 -0500

    added my major to the output

--- hello.c
+++ hello.c
@@ -1,1 +1,19 @@
+/**
+ * Author: Isaac Trausch!
+ *         isaactrausch@gmail.com
+ * Date: 2026/25/08
+ * Major: Computer Engineering
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
 
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+  printf("I love the School of Computing!\n");
+
+  return 0;
+}
